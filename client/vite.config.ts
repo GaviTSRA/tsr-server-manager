ed6f18342c66684ef0df974a67020df62f181863
GaviTSRA <gavitsra@gmail.com>
2025-01-18 10:44:05 +0100

    fix vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,22 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { readFileSync } from "fs";
 
-let https = {};
-if (process.env.HTTPS === "true") {
-  https = {
-    key: readFileSync("private-key.pem"),
-    cert: readFileSync("certificate.pem"),
-  };
-  console.info("HTTPS Enabled");
-}
-
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    strictPort: true,
-    port: parseInt(process.env.PORT ?? "3001"),
-    https,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  let https = {};
+
+  if (env.HTTPS === "true") {
+    https = {
+      key: readFileSync("private-key.pem"),
+      cert: readFileSync("certificate.pem"),
+    };
+    console.info("HTTPS Enabled");
+  }
+
+  return {
+    plugins: [react()],
+    server: {
+      strictPort: true,
+      port: parseInt(env.PORT ?? "3001"),
+      https,
+    },
+  };
 });
