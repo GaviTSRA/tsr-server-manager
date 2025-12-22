5abdf56f2ce775c73bb8d6567d8e99d9e93a3020
GaviTSRA <gavitsra@gmail.com>
2025-12-22 20:29:20 +0100

    More fixes

--- node/servertypes/python/install.sh
+++ node/servertypes/python/install.sh
@@ -1,10 +1,7 @@
 if [ -n "$PACKAGES" ]; then
-   # install python packagess in venv
-   # the venv may not exist yet, in which case it needs to be created first (checked using dir )
    if [ ! -d "venv" ]; then
       python3 -m venv venv
    fi
 
-   source venv/bin/activate
-   pip install $PACKAGES
+   venv/bin/pip install $PACKAGES
 fi
